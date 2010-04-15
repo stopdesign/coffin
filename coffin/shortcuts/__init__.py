dd3327cc2bd3dfec0475f28d8d04f411c3cafbf4
Gregory <gregory@design.ru>
2010-04-15 18:26:37 +0400

    Added 'mimetype', 'status' and 'content_type' options to 'render_to_response'.

--- coffin/shortcuts/__init__.py
+++ coffin/shortcuts/__init__.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponse
 
 # Merge with original namespace so user
@@ -13,13 +14,16 @@ __all__ = ('render_to_string', 'render_to_response',)
 from coffin.template.loader import render_to_string
 
 
-def render_to_response(template_name, dictionary=None, context_instance=None,
-                       mimetype=None):
+def render_to_response(template_name, context=None, context_instance=None,
+                       mimetype=None, status=200, content_type=None):
     """
     :param template_name: Filename of the template to get or a sequence of
         filenames to try, in order.
-    :param dictionary: Rendering context for the template.
+    :param context: Rendering context for the template.
     :returns: A response object with the evaluated template as a payload.
     """
-    rendered = render_to_string(template_name, dictionary, context_instance)
-    return HttpResponse(rendered, mimetype=mimetype)
+    if content_type is None:
+        content_type = getattr(settings, 'DEFAULT_CONTENT_TYPE', 'text/html')
+    content = render_to_string(template_name, context, context_instance)
+    return HttpResponse(content, mimetype=mimetype, status=status,
+        content_type=content_type)
